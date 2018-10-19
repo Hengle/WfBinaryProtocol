4ae8c3d63e2c5bb7c4819a39af299da7ef1e9153
qq576067421 <9191819+qq576067421@users.noreply.github.com>
2018-10-19 18:39:21 +0800

    NetCore

---
+++ NetCore/TestWfBinaryProtocol/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace TestWfBinaryProtocol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test app = new Test();
+            app.Start();
+            while(true)
+            {
+                bool run = app.Update();
+                if(run == false )
+                {
+                    break;
+                }
+                Thread.Sleep(1000);
+            }
+
+            app.OnDestroy();
+        }
+    }
+}
---
+++ NetCore/TestWfBinaryProtocol/src/CSHotFixNet/DataBytes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class DataBytes:PooledClassObject
+{
+    private byte[] Bytes = null;
+    //有效长度
+    private int Length = 0;
+    public override void New(object param)
+    {
+        Length = 0;
+    }
+    public override void Delete()
+    {
+        NetworkManager.GetBytePool().Return(Bytes, true);
+        Bytes = null;
+        Length = 0;
+    }
+    public void SetBytes(int length)
+    {
+        Length = length;
+        Bytes = NetworkManager.GetBytePool().Rent(length);
+    }
+    public byte[] GetBytes()
+    {
+        return Bytes;
+    }
+    public int GetLength()
+    {
+        return Length;
+    }
+    public override void DestroyClass()
+    {
+        PooledClassManager<DataBytes>.DeleteClass(this);
+    }
+}
+
